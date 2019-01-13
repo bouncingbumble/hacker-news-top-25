31f3e8ae1ea08b97531089c8adc4d0ca36dba2cf
Jordan Boudreau <jordboudreau@gmail.com>
2019-01-13 14:52:44 -0800

    add initial api calls to hacker new

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stories: []
+    };
+  }
+
+  componentDidMount(){
+    const topStoriesUrl = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+    const storyUrlBase = 'https://hacker-news.firebaseio.com/v0/item/';
+
+    fetch(topStoriesUrl)
+    .then(data => data.json())
+    .then(data => data.map(id => {
+      const url = `${storyUrlBase}${id}.json`;
+      return fetch(url)
+              .then(d => d.json());
+    }))
+    .then(promises => Promise.all(promises))
+    .then(stories => this.setState({stories}))
+  }
+
   render() {
+    let views = <div>Loading..{JSON.stringify(this.state.stories)}.</div>
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Hacker News Top Stories</h2>
+        {views}
       </div>
     );
   }
